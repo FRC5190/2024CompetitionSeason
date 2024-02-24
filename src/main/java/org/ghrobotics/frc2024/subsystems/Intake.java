2aed196ad5eda1b80a98a89881cd10c3d4c12a30
Jake-GHHS <ironwelder1234@gmail.com>
2024-02-24 13:46:33 -0500

    UpdatedIntakeFeb24
    Changed code to work with 2 motors(i hope)

--- src/main/java/org/ghrobotics/frc2024/subsystems/Intake.java
+++ src/main/java/org/ghrobotics/frc2024/subsystems/Intake.java
@@ -9,7 +9,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
   // Motor Controllers
-  private final CANSparkMax leader_;
+  private final CANSparkMax leader_left_;
+  private final CANSparkMax leader_right_;
   private final PeriodicIO io_ = new PeriodicIO();
 
   private final PIDController controller_ = new PIDController(Constants.kP, 0, 0);
@@ -17,16 +18,26 @@ public class Intake extends SubsystemBase {
   // Constructor
   public Intake() {
     // Initialize motor controllers
-    leader_ = new CANSparkMax(Constants.kLeaderId, MotorType.kBrushless);
-    leader_.restoreFactoryDefaults();
-    leader_.setInverted(true);
-    leader_.setIdleMode(CANSparkMax.IdleMode.kCoast);
+    leader_left_ = new CANSparkMax(Constants.kLeaderId, MotorType.kBrushless);
+    leader_left_.restoreFactoryDefaults();
+    leader_left_.setInverted(true);
+    leader_left_.setIdleMode(CANSparkMax.IdleMode.kCoast);
+
+
+    leader_right_ = new CANSparkMax(Constants.kLeaderId, MotorType.kBrushless);
+    leader_right_.restoreFactoryDefaults();
+    leader_right_.setInverted(false);
+    leader_right_.setIdleMode(CANSparkMax.IdleMode.kCoast);
+    leader_right_.follow(leader_left_);
+
+    
   }
   
   public void periodic() {
     // Read inputs.
-    io_.current = leader_.getOutputCurrent();
-    leader_.set(io_.demand);
+    io_.current_left_ = leader_left_.getOutputCurrent();
+    io_.current_right_ = leader_right_.getOutputCurrent();
+    leader_left_.set(io_.demand);
   }
 
   public double getPercent() {
@@ -41,7 +52,8 @@ public class Intake extends SubsystemBase {
   // IO
   public static class PeriodicIO {
     // Input
-    double current;
+    double current_left_;
+    double current_right_;
 
     // Output
     double demand;
